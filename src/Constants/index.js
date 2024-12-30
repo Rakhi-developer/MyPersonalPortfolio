a05214ea38e43b5c143267893c98961facc9d22f
rajat <fzdsharmarajat@gmail.com>
2024-12-30 22:42:31 +0530

    .

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -1,7 +1,7 @@
-import project1 from "../assets/projects/project1.jpg";
-import project2 from "../assets/projects/project2.jpg";
-import project3 from "../assets/projects/project3.jpg";
-import project4 from "../assets/projects/project4.jpg";
+import project1 from "../assets/Projects/project1.jpg";
+import project2 from "../assets/Projects/project2.jpg";
+import project3 from "../assets/Projects/project3.jpg";
+import project4 from "../assets/Projects/project4.jpg";
 
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
